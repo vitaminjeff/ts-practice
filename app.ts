ed50d0432dd02fafa690a5b57ed5f6a7eb354e0a
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-29 03:37:33 -0800

    Use union types and runtime type checking

--- app.ts
+++ app.ts
@@ -1,69 +1,15 @@
-// console.log('Time to get started...');
-
-function add(n1: number, n2: number, showResult: boolean, phrase: string) {
-  // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-  //     throw new Error('Incorrect input!');
-  // }
-
-  const result = n1 + n2;
-  if (showResult) {
-    console.log(phrase + result);
+function combine(input1: number | string, input2: number | string) {
+  let result;
+  if (typeof input1 === "number" && typeof input2 === "number") {
+    result = input1 + input2;
   } else {
-    return n1 + n2;
+    result = input1.toString() + input2.toString();
   }
+  return result;
 }
 
-// let number1: number = 5; // redundant
-// let number1: number; // okay if not initialized
-let number1 = 5; // same as 5.0, use type inference
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
 
-const number2 = 2.8;
-const printResult = true;
-let resultPhrase = "Result is: ";
-// resultPhrase = 0; // build time error
-
-const result = add(number1, number2, printResult, resultPhrase);
-
-// objects
-
-// const person: {
-//   name: string;
-//   age: number;
-//   hobbies: string[];
-//   role: [number, string]; // tuples
-// } = {
-//   name: "Maximilian",
-//   age: 30,
-//   hobbies: ["Sports", "Cooking"],
-//   role: [2, "author"],
-// };
-
-enum Role {
-  ADMIN = 5,
-  READ_ONLY = 100,
-  AUTHOR = "sometext",
-}
-
-const person = {
-  name: "Maximilian",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  role: Role.ADMIN,
-};
-
-// person.role.push("admin"); // shouldn't allow this, but typescript doesn't allow this
-// person.role[1] = 10; // supported type check
-// person.role = [0, 'admin', 'user']; // supported type check
-
-let favoriteActiviites: string[];
-favoriteActiviites = ["Sports"];
-
-console.log(person.name);
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
-  // console.log(hobby.map()); // ERROR
-}
-
-if (person.role === Role.ADMIN) {
-  console.log("is admin");
-}
+const combinedNames = combine("Jack", "Jill");
+console.log(combinedNames);
---
+++ basics.ts
@@ -0,0 +1,69 @@
+// console.log('Time to get started...');
+
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+  // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
+  //     throw new Error('Incorrect input!');
+  // }
+
+  const result = n1 + n2;
+  if (showResult) {
+    console.log(phrase + result);
+  } else {
+    return n1 + n2;
+  }
+}
+
+// let number1: number = 5; // redundant
+// let number1: number; // okay if not initialized
+let number1 = 5; // same as 5.0, use type inference
+
+const number2 = 2.8;
+const printResult = true;
+let resultPhrase = "Result is: ";
+// resultPhrase = 0; // build time error
+
+const result = add(number1, number2, printResult, resultPhrase);
+
+// objects
+
+// const person: {
+//   name: string;
+//   age: number;
+//   hobbies: string[];
+//   role: [number, string]; // tuples
+// } = {
+//   name: "Maximilian",
+//   age: 30,
+//   hobbies: ["Sports", "Cooking"],
+//   role: [2, "author"],
+// };
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY = 100,
+  AUTHOR = "sometext",
+}
+
+const person = {
+  name: "Maximilian",
+  age: 30,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
+};
+
+// person.role.push("admin"); // shouldn't allow this, but typescript doesn't allow this
+// person.role[1] = 10; // supported type check
+// person.role = [0, 'admin', 'user']; // supported type check
+
+let favoriteActiviites: string[];
+favoriteActiviites = ["Sports"];
+
+console.log(person.name);
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // ERROR
+}
+
+if (person.role === Role.ADMIN) {
+  console.log("is admin");
+}
