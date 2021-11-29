239ba88667ec5d546996ca93552757159a8d688d
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-29 01:40:39 -0800

    Add more function parameters and types

--- app.ts
+++ app.ts
@@ -1,15 +1,21 @@
 // console.log('Time to get started...');
 
-function add(n1: number, n2: number) {
-    if (typeof n1 !== 'number' && typeof n2 !== 'number') {
-        throw new Error('Incorrect input!');
-    }
+function add(n1: number, n2: number, showResult: boolean, phrase: string) {
+    // if (typeof n1 !== 'number' && typeof n2 !== 'number') {
+    //     throw new Error('Incorrect input!');
+    // }
     
-    return n1 + n2;
+    const result = n1 + n2;
+    if(showResult) {
+        console.log(phrase + result);
+    } else {
+        return n1 + n2;
+    }
 }
 
-const number1 = '5';
+const number1 = 5; // same as 5.0
 const number2 = 2.8;
+const printResult = true;
+const resultPhrase = 'Result is: '
 
-const result = add(number1, number2);
-console.log(result);
+const result = add(number1, number2, printResult, resultPhrase);
