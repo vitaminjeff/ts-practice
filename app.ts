d230fac351ab2e280cf6eee12fd0c22347a395b0
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2021-11-29 01:52:35 -0800

    Demonstrate violating type inference

--- app.ts
+++ app.ts
@@ -13,9 +13,13 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
     }
 }
 
-const number1 = 5; // same as 5.0
+// let number1: number = 5; // redundant
+// let number1: number; // okay if not initialized
+let number1 = 5; // same as 5.0, use type inference
+
 const number2 = 2.8;
 const printResult = true;
-const resultPhrase = 'Result is: '
+let resultPhrase = 'Result is: '
+resultPhrase = 0;
 
 const result = add(number1, number2, printResult, resultPhrase);
